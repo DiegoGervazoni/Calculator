38283a2a783e554bceac3e69b3aa6d92e971cb97
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2023-03-29 09:56:47 -0300

    Implementacao e refinamento do codigo

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -9,17 +9,16 @@ public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
-        Calculator calc = new Calculator(); //instanciando o objeto Calculator
 
         System.out.print("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = calc.circumference(radius);
-        double v = calc.volume(radius);
+        double c = Calculator.circumference(radius);
+        double v = Calculator.volume(radius);
 
         System.out.printf("Circumference: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI value: %.2f%n", calc.PI);
+        System.out.printf("PI value: %.2f%n", Calculator.PI);
 
         sc.close();
     }
